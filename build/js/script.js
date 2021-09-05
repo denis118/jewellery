bc57ee480f88a26995e20bde59971ee40d803ad2
Denis Tsap <ts.dios.v@gmail.com>
2021-09-05 16:28:21 +0300

    Верстает страницу login

--- build/js/script.js
+++ build/js/script.js
@@ -193,179 +193,179 @@
 
   findHeader();
 
-  if (header) {
-    var manageHeader = function () {
-      var that = {};
-
-      that.activate = function () {
-        this.header = header;
-        this.burger = this.header.querySelector('.header__burger');
-        this.lowerContainer = this.header.querySelector('.header__container--lower');
-        this.body = document.body;
-
-        this.header.classList.add('header--js');
-        this.isShown = false;
-        this.toggleMargin();
-        this.burger.addEventListener('click', this.onBurgerClick);
-        return this;
-      };
-
-      that.toggleMargin = function () {
-        var nextSibling = this.header.nextElementSibling
-          ? this.header.nextElementSibling
-          : null;
-
-        var main = document.querySelector('main')
-          ? document.querySelector('main')
-          : null;
-
-        switch (isPreDesktopWidth()) {
-          case true:
-            if (nextSibling && main && Object.is(nextSibling, main)) {
-              var height = this.header.scrollHeight;
-              main.style.marginTop = height + UNITS;
-            }
-
-            break;
-
-          case false:
-            if (nextSibling && main && Object.is(nextSibling, main)) {
-              main.style.marginTop = 0;
-            }
-
-            break;
-
-          default:
-            break;
-        }
-      };
-
-      that.setAttributes = function () {
-        if (isPreDesktopWidth()) {
-          setAttributes(this.header);
-          this.lowerContainer.setAttribute('tabindex', '-1');
-        }
-
-        return this;
-      };
-
-      that.resetAttributes = function () {
-        if (!isPreDesktopWidth()) {
-          resetAttributes(this.header);
-          this.lowerContainer.removeAttribute('tabindex');
-        }
-      };
-
-      that.show = function () {
-        this.isShown = true;
-        this.previouslyFocused = document.activeElement;
-
-        this.body.classList.add('scroll-stop');
-        this.header.classList.add('menu-open');
-
-        this.setEventListeners();
-        moveFocusIn(this.header);
-      };
-
-      that.hide = function () {
-        if (this.previouslyFocused && this.previouslyFocused.focus) {
-          this.previouslyFocused.focus();
-        }
-
-        this.isShown = false;
-        this.body.classList.remove('scroll-stop');
-        this.header.classList.remove('menu-open');
-
-        this.eraseEventListeners();
-      };
-
-      that.onBurgerClick = function () {
-        if (!that.isShown) {
-          that.show();
-        } else {
-          that.hide();
-        }
-      };
-
-      that.onBodyFocus = function (evt) {
-        onBodyFocus(evt, that.header);
-      };
-
-      that.onDocumentKeyDown = function (evt) {
-        if (isTabEvent(evt)) {
-          trapTabKey(that.header, evt);
-        }
-      };
-
-      that.setEventListeners = function () {
-        document.addEventListener('keydown', this.onDocumentKeyDown);
-        this.body.addEventListener('focus', this.onBodyFocus, true);
-      };
-
-      that.eraseEventListeners = function () {
-        document.removeEventListener('keydown', this.onDocumentKeyDown);
-        this.body.removeEventListener('focus', this.onBodyFocus, true);
-      };
-
-      that.destroy = function () {
-        this.burger.removeEventListener('click', this.onBurgerClick);
-        this.body.removeEventListener('focus', this.onBodyFocus, true);
-        document.removeEventListener('keydown', this.onDocumentKeyDown);
-      };
-
-      return that;
-    };
-
-    var isPreDesktopWidth = window.utility.isPreDesktopWidth;
-    var isTabEvent = window.utility.isTabEvent;
-    var setAttributes = window.utility.setAttributes;
-    var resetAttributes = window.utility.resetAttributes;
-    var moveFocusIn = window.utility.moveFocusIn;
-    var trapTabKey = window.utility.trapTabKey;
-    var onBodyFocus = window.utility.onBodyFocus;
-
-    var headerManager = manageHeader();
-    headerManager
-        .activate()
-        .setAttributes();
-
-    var onWindowResize = (function () {
-      var isWorkedOnPreDesktopWidth = false;
-      var isWorkedOnDesktopWidth = false;
-
-      return function () {
-        if (!isPreDesktopWidth() && !isWorkedOnDesktopWidth) {
-          headerManager.resetAttributes();
-          headerManager.hide();
-          isWorkedOnPreDesktopWidth = false;
-          isWorkedOnDesktopWidth = true;
-          return;
-        }
-
-        if (isPreDesktopWidth() && !isWorkedOnPreDesktopWidth) {
-          headerManager.setAttributes();
-          isWorkedOnPreDesktopWidth = true;
-          isWorkedOnDesktopWidth = false;
-        }
-
-        headerManager.toggleMargin();
-      };
-    })();
-
-    var onWindowBeforeunload = function () {
-      headerManager.destroy();
-      window.removeEventListener('resize', onWindowResize);
-      // window.removeEventListener('beforeunload', onWindowBeforeunload);
-    };
-
-    window.addEventListener('resize', onWindowResize);
-    // window.addEventListener('beforeunload', onWindowBeforeunload);
-
-    // export
-    window.headerDestroyer = {
-      onWindowBeforeunload: onWindowBeforeunload
-    };
+  if (!header) {
+    return;
   }
+
+  var manageHeader = function () {
+    var that = {};
+
+    that.activate = function () {
+      this.header = header;
+      this.burger = this.header.querySelector('.header__burger');
+      this.lowerContainer = this.header.querySelector('.header__container--lower');
+      this.body = document.body;
+
+      this.header.classList.add('header--js');
+      this.isShown = false;
+      this.toggleMargin();
+      this.burger.addEventListener('click', this.onBurgerClick);
+      return this;
+    };
+
+    that.toggleMargin = function () {
+      var nextSibling = this.header.nextElementSibling
+        ? this.header.nextElementSibling
+        : null;
+
+      var main = document.querySelector('main')
+        ? document.querySelector('main')
+        : null;
+
+      switch (isPreDesktopWidth()) {
+        case true:
+          if (nextSibling && main && Object.is(nextSibling, main)) {
+            var height = this.header.scrollHeight;
+            main.style.marginTop = height + UNITS;
+          }
+
+          break;
+
+        case false:
+          if (nextSibling && main && Object.is(nextSibling, main)) {
+            main.style.marginTop = 0;
+          }
+
+          break;
+
+        default:
+          break;
+      }
+    };
+
+    that.setAttributes = function () {
+      if (isPreDesktopWidth()) {
+        setAttributes(this.header);
+        this.lowerContainer.setAttribute('tabindex', '-1');
+      }
+
+      return this;
+    };
+
+    that.resetAttributes = function () {
+      if (!isPreDesktopWidth()) {
+        resetAttributes(this.header);
+        this.lowerContainer.removeAttribute('tabindex');
+      }
+    };
+
+    that.show = function () {
+      this.isShown = true;
+      this.previouslyFocused = document.activeElement;
+
+      this.body.classList.add('scroll-stop');
+      this.header.classList.add('menu-open');
+
+      this.setEventListeners();
+      moveFocusIn(this.header);
+    };
+
+    that.hide = function () {
+      if (this.previouslyFocused && this.previouslyFocused.focus) {
+        this.previouslyFocused.focus();
+      }
+
+      this.isShown = false;
+      this.body.classList.remove('scroll-stop');
+      this.header.classList.remove('menu-open');
+
+      this.eraseEventListeners();
+    };
+
+    that.onBurgerClick = function () {
+      if (!that.isShown) {
+        that.show();
+      } else {
+        that.hide();
+      }
+    };
+
+    that.onBodyFocus = function (evt) {
+      onBodyFocus(evt, that.header);
+    };
+
+    that.onDocumentKeyDown = function (evt) {
+      if (isTabEvent(evt)) {
+        trapTabKey(that.header, evt);
+      }
+    };
+
+    that.setEventListeners = function () {
+      document.addEventListener('keydown', this.onDocumentKeyDown);
+      this.body.addEventListener('focus', this.onBodyFocus, true);
+    };
+
+    that.eraseEventListeners = function () {
+      document.removeEventListener('keydown', this.onDocumentKeyDown);
+      this.body.removeEventListener('focus', this.onBodyFocus, true);
+    };
+
+    that.destroy = function () {
+      this.burger.removeEventListener('click', this.onBurgerClick);
+      this.body.removeEventListener('focus', this.onBodyFocus, true);
+      document.removeEventListener('keydown', this.onDocumentKeyDown);
+    };
+
+    return that;
+  };
+
+  var isPreDesktopWidth = window.utility.isPreDesktopWidth;
+  var isTabEvent = window.utility.isTabEvent;
+  var setAttributes = window.utility.setAttributes;
+  var resetAttributes = window.utility.resetAttributes;
+  var moveFocusIn = window.utility.moveFocusIn;
+  var trapTabKey = window.utility.trapTabKey;
+  var onBodyFocus = window.utility.onBodyFocus;
+
+  var headerManager = manageHeader();
+  headerManager
+      .activate()
+      .setAttributes();
+
+  var onWindowResize = (function () {
+    var isWorkedOnPreDesktopWidth = false;
+    var isWorkedOnDesktopWidth = false;
+
+    return function () {
+      if (!isPreDesktopWidth() && !isWorkedOnDesktopWidth) {
+        headerManager.resetAttributes();
+        headerManager.hide();
+        isWorkedOnPreDesktopWidth = false;
+        isWorkedOnDesktopWidth = true;
+        return;
+      }
+
+      if (isPreDesktopWidth() && !isWorkedOnPreDesktopWidth) {
+        headerManager.setAttributes();
+        isWorkedOnPreDesktopWidth = true;
+        isWorkedOnDesktopWidth = false;
+      }
+
+      headerManager.toggleMargin();
+    };
+  })();
+
+  window.addEventListener('resize', onWindowResize);
+
+  var onWindowBeforeunload = function () {
+    headerManager.destroy();
+    window.removeEventListener('resize', onWindowResize);
+  };
+
+  // export
+  window.headerDestroyer = {
+    onWindowBeforeunload: onWindowBeforeunload
+  };
 })();
 
 //
@@ -757,54 +757,54 @@
 
   findSliders();
 
-  if (sliders && sliders.length) {
-    sliders.forEach(function (it) {
-      var slider = initSlider(it);
-      slider.activate().setEventListeners();
-      window.slider[slider.root.id] = slider;
-    });
-
-    var rebuild = useMethod('slider', 'rebuild');
-    var manageNumbers = useMethod('slider', 'manageNumbers');
-
-    var onWindowResize = (function () {
-      var isWorkedOnPreDesktopWidth = false;
-      var isWorkedOnDesktopWidth = false;
-      var currentMode = '';
-
-      return function () {
-        currentMode = getCurrentMode();
-
-        if (currentMode !== mode && firstLoading) {
-          firstLoading = false;
-        }
-
-        if (isPreDesktopWidth()) {
-          manageNumbers();
-        }
-
-        if (!isPreDesktopWidth() && !isWorkedOnDesktopWidth && !firstLoading) {
-          rebuild();
-          isWorkedOnPreDesktopWidth = false;
-          isWorkedOnDesktopWidth = true;
-          return;
-        }
-
-        if (isPreDesktopWidth() && !isWorkedOnPreDesktopWidth && !firstLoading) {
-          rebuild();
-          isWorkedOnPreDesktopWidth = true;
-          isWorkedOnDesktopWidth = false;
-        }
-      };
-    })();
-
-    var onWindowBeforeunload = useMethod('slider', 'eraseEventListeners');
-
-    window.addEventListener('resize', onWindowResize);
-    // window.addEventListener('beforeunload', onWindowBeforeunload);
-
-    firstLoading = true;
+  if (!(sliders && sliders.length)) {
+    return;
   }
+
+  sliders.forEach(function (it) {
+    var slider = initSlider(it);
+    slider.activate().setEventListeners();
+    window.slider[slider.root.id] = slider;
+  });
+
+  var rebuild = useMethod('slider', 'rebuild');
+  var manageNumbers = useMethod('slider', 'manageNumbers');
+
+  var onWindowResize = (function () {
+    var isWorkedOnPreDesktopWidth = false;
+    var isWorkedOnDesktopWidth = false;
+    var currentMode = '';
+
+    return function () {
+      currentMode = getCurrentMode();
+
+      if (currentMode !== mode && firstLoading) {
+        firstLoading = false;
+      }
+
+      if (isPreDesktopWidth()) {
+        manageNumbers();
+      }
+
+      if (!isPreDesktopWidth() && !isWorkedOnDesktopWidth && !firstLoading) {
+        rebuild();
+        isWorkedOnPreDesktopWidth = false;
+        isWorkedOnDesktopWidth = true;
+        return;
+      }
+
+      if (isPreDesktopWidth() && !isWorkedOnPreDesktopWidth && !firstLoading) {
+        rebuild();
+        isWorkedOnPreDesktopWidth = true;
+        isWorkedOnDesktopWidth = false;
+      }
+    };
+  })();
+
+  window.addEventListener('resize', onWindowResize);
+  firstLoading = true;
+
+  var onWindowBeforeunload = useMethod('slider', 'eraseEventListeners');
 
   // export
   window.sliderDestroyer = {
@@ -905,16 +905,17 @@
 
   findAccordeons();
 
-  if (accordeons && accordeons.length) {
-    accordeons.forEach(function (it) {
-      var accordeon = initAccordeon(it);
-      accordeon.activate().setEventListener();
-      window.accordeon[accordeon.root.id] = accordeon;
-    });
-
-    var onWindowBeforeunload = useMethod('accordeon', 'eraseEventListener');
-    // window.addEventListener('beforeunload', onWindowBeforeunload);
+  if (!(accordeons && accordeons.length)) {
+    return;
   }
+
+  accordeons.forEach(function (it) {
+    var accordeon = initAccordeon(it);
+    accordeon.activate().setEventListener();
+    window.accordeon[accordeon.root.id] = accordeon;
+  });
+
+  var onWindowBeforeunload = useMethod('accordeon', 'eraseEventListener');
 
   // export
   window.accordeonDestroyer = {
@@ -1093,14 +1094,12 @@
     };
   })();
 
+  window.addEventListener('resize', onWindowResize);
+
   var onWindowBeforeunload = function () {
     filterManager.destroy();
     window.removeEventListener('resize', onWindowResize);
-    // window.removeEventListener('beforeunload', onWindowBeforeunload);
   };
-
-  window.addEventListener('resize', onWindowResize);
-  // window.addEventListener('beforeunload', onWindowBeforeunload);
 
   // export
   window.filterDestroyer = {
@@ -1160,8 +1159,6 @@
     filterCleaner.removeEventListener('click', onFilterCleanerClick);
     window.removeEventListener('beforeunload', onWindowBeforeunload);
   };
-
-  // window.addEventListener('beforeunload', onWindowBeforeunload);
 
   // export
   window.filterCleanerDestroyer = {
@@ -1322,10 +1319,7 @@
 
   var onWindowBeforeunload = function () {
     loginManager.destroy();
-    // window.removeEventListener('beforeunload', onWindowBeforeunload);
   };
-
-  // window.addEventListener('beforeunload', onWindowBeforeunload);
 
   // export
   window.loginDestroyer = {
@@ -1347,9 +1341,10 @@
       'filterCleanerDestroyer',
       'loginDestroyer'
     ].forEach(function (item) {
-      console.log('in');
       window[item].onWindowBeforeunload();
     });
+
+    window.removeEventListener('beforeunload', onWindowBeforeunload);
   };
 
   window.addEventListener('beforeunload', onWindowBeforeunload);
